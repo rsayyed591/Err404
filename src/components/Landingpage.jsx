f25f1137a9dce1bd57d3766674157386548421c0
rsayyed591 <rehansayyed591@gmail.com>
2024-08-30 14:43:40 +0530

    Video Background

---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,22 @@
+import '../css/landingpage.css'
+import BgVideo from '../assets/bg3.mp4'
+
+const Landingpage = () => {
+    return (
+        <div className="landingpage">
+
+            <video src={BgVideo} autoPlay muted loop className="video-bg" />
+            <div className="bg-overlay"></div>
+
+            <div className="home-text">
+                <h1>Err 404 Hackathon</h1>
+                <p>Innovations Roar, Errors No more</p>
+            </div>
+
+            <div className="home-btn">Devfolio</div>
+
+        </div>
+    )
+}
+
+export default Landingpage;
